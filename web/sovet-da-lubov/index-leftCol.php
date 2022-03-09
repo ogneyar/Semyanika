c6199357d648928b73b94eaad885df4c3705b1bd
ogneyar <ogneyar-ne@ya.ru>
2022-03-09 15:37:39 +0300

    cool

---
+++ web/sovet-da-lubov/index-leftCol.php
@@ -0,0 +1,46 @@
+﻿<article id="sovet-da-lubov">
+<div>
+
+<center>
+<h3>
+Совет-да-любовь&#10084;
+</h3><br />
+</center>
+
+<h4>
+&nbsp;&nbsp;&nbsp;Пусть каждый найдёт своё семейное счастье, где бы оно не пряталось!
+</h4><br />
+<h3>
+&nbsp;&nbsp;&nbsp;И, семейное,  и, личное, в содружестве, уважении, любви со своим партнёром!
+</h3><br />
+<h3>
+&nbsp;&nbsp;&nbsp;Мы рады вам в этом помочь.
+</h3><br />
+<h3>
+&nbsp;&nbsp;&nbsp;Для нас очень важен результат нашей совместной деятельности и поэтому мы вам рады!
+</h3><br />
+<h3>
+&nbsp;&nbsp;&nbsp;Благодарны за ваши отзывы.
+</h3><br />
+<h3>
+&nbsp;&nbsp;&nbsp;Несмотря на то, что многие сложившиеся пары, не хотят распространяться о своём  счастье, все же, мы ждём ваших добрых откликов.
+</h3><br />
+<h3>
+&nbsp;&nbsp;&nbsp;С уважением и любовью, руководитель Центра Оксана Семьяника
+</h3><br />
+
+<img src="/sovet-da-lubov/photo_34704.jpg">
+<br><br>
+
+<center>
+<video width="100%" controls="controls" poster="Anna_Andrey.jpg" controlsList="nodownload">
+	<source src="Anna_Andrey.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
+	Тег video не поддерживается вашим браузером. 
+</video>
+<video width="100%" controls="controls" poster="Elena_Ivan.jpg" controlsList="nodownload">
+	<source src="Elena_Ivan.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
+	Тег video не поддерживается вашим браузером. 
+</video>
+</center>
+</div>
+</article>
