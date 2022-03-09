2de3c4ac8e9cd0bc0ba72104e179816fc623253e
ogneyar <ogneyar-ne@ya.ru>
2022-03-09 15:36:02 +0300

    yuf

---
+++ web/partneri/index-leftCol.php
@@ -0,0 +1,96 @@
+<article id="otzivi">
+<div>
+
+<center>
+<h3>
+Наши партнёры!
+</h3><br />
+</center>
+
+<h4>
+&nbsp;&nbsp;&nbsp;&nbsp;Ольга Никитина - практикующий дефектолог, логопед -психолог, руководитель клуба для женщин "Любодающие".<br>
+<a href="https://wa.me/79185067790" target="_blank">
+WhatsApp +7(918)506-77-90
+</a>
+</h4><br>
+<h4>
+&nbsp;&nbsp;&nbsp;&nbsp;Инна Ляхова - практикующий психолог, аккредитованный член общества практикующих психологов, учредитель проекта "Интеграция".<br>
+<a href="https://instagram.com/integrationcentr" target="_blank">
+https://instagram.com/integrationcentr
+</a>
+</h4><br>
+<h4>
+&nbsp;&nbsp;&nbsp;&nbsp;Ирина Семеренко - сертифицированный мастер трансформационных, финансово- бизнесовых игр, финансовый консультант.<br>
+<a href="https://instagram.com/intellect_game" target="_blank">
+https://instagram.com/intellect_game
+</a>
+</h4><br>
+<h4>
+&nbsp;&nbsp;&nbsp;&nbsp;Брачные агентства и
+Туристические персональные гиды в Ростове н/Д, России.
+</h4><br>
+<h4>
+&nbsp;&nbsp;&nbsp;&nbsp;Лилия Иванова - персональный гид, фотохудожник.<br>
+<a href="https://instagram.com/liliafoto1477?r=nametag" target="_blank">
+https://instagram.com/liliafoto1477?r=nametag
+</a>
+</h4><br>
+<h4>
+&nbsp;&nbsp;&nbsp;&nbsp;Александр Иванов - деловой туризм, автодилер.
+</h4><br>
+<h4>
+&nbsp;&nbsp;&nbsp;&nbsp;Клуб знакомств для православных, а так-же, казаков и казачек.<br>
+<a href="https://chat.whatsapp.com/IzkAp4QI37fIUO1scOj66Z" target="_blank">
+https://chat.whatsapp.com/IzkAp4QI37fIUO1scOj66Z
+</a><br>
+Атаман +7(905)820-55-45
+</h4><br>
+<h4>
+&nbsp;&nbsp;&nbsp;&nbsp;Ирина Карпова - руководитель, педагог Воскресной школы храма Донской Иконы Божией Матери.<br>
+<a href="https://wa.me/79081710508" target="_blank">
+WhatsApp +7(908)171-05-08
+</a>
+</h4><br>
+<h4>
+&nbsp;&nbsp;&nbsp;&nbsp;Никита Фомичев -  Гончарная мастерская "Лукоморье".<br>
+<a href="https://vk.com/lukomorie_art" target="_blank">
+https://vk.com/lukomorie_art
+</a>
+</h4><br>
+<h4>
+&nbsp;&nbsp;&nbsp;&nbsp;Елена Фортушнова - художник- педагог.<br>
+<a href="https://linktr.ee/elfortart" target="_blank">
+https://linktr.ee/elfortart
+</a>
+</h4><br>
+<h4>
+&nbsp;&nbsp;&nbsp;&nbsp;Елена Волкова - стилист - парикмахер.
+</h4><br>
+<h4>
+&nbsp;&nbsp;&nbsp;&nbsp;Юлия Мижерицкая - косметолог.<br>
+<a href="https://instagram.com/p/CBwIOi3s5RL/?igshid=1mhra9lxf0b0q" target="_blank">
+https://instagram.com/p/CBwIOi3s5RL/?igshid=1mhra9lxf0b0q
+</a>
+</h4><br>
+<h4>
+&nbsp;&nbsp;&nbsp;&nbsp;Огънеяръ - программист.<br>
+<a href="https://t.me/ogneyar_ya" target="_blank">
+Ссылка на профиль в телеграм &#128072;
+</a>
+</h4><br><br><br>
+
+<h4>
+&nbsp;&nbsp;&nbsp;Благодарю  партнеров, за наше сотворчество в общем деле, на благо народу!
+</h4><br>
+<h4>
+&nbsp;&nbsp;&nbsp;Дальнейших профессиональных успехов вам и личного счастья!
+</h4><br>
+<h4>
+&nbsp;&nbsp;&nbsp;Наши двери всегда открыты новым возможностям, добро пожаловать!
+</h4><br>
+<h4>
+&nbsp;&nbsp;&nbsp;С уважением и благодарностью, руководитель центра, Оксана Семьяника.
+</h4>
+
+</div>
+</article>
