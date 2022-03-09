2fdb05122d51655879483b75e40c140d092111b5
ogneyar <ogneyar-ne@ya.ru>
2022-03-09 15:34:12 +0300

    eeee

---
+++ web/contacts/index-leftCol.php
@@ -0,0 +1,55 @@
+﻿<article id="contacts">
+<div>
+	
+<h3>
+<a href="https://ok.ru/profile/329270985047" target="_blank">
+Мы в ОдноКлассниках!
+</a>
+ - заходите в гости.
+</h3><br />
+	
+<h3>
+<a href="https://www.facebook.com/profile.php?id=100011374764032" target="_blank">
+Мы в facebook!
+</a>
+ - добро пожаловать!
+ </h3><br /><br />
+	
+<h3>
+<a href="https://www.youtube.com/channel/UCZOunpCQtO2J2Ro5A3BkswA" target="_blank">
+Наш YouTube канал!
+</a>
+ - милости просим.
+ </h3><br /><br />
+	
+<h3>
+<a href="https://vk.com/id303326087" target="_blank">
+Мы в ВКокнтакте!
+</a>
+ - заходите в гости.
+</h3><br /><br />
+	
+<h3>
+<a href="https://wa.me/79185770009" target="_blank">
+Вотсап!
+</a>
+ - добро пожаловать!
+</h3><br /><br />
+	
+<h3>
+<a href="https://t.me/oksanasemyanika" target="_blank">
+Телеграм!
+</a>
+ - милости просим.
+</h3><br /><br />
+	
+<h3>
+oksana7yanika@mail.ru - отправляйте свои письма на этот почтовый ящик.
+</h3><br /><br />
+	
+<h1>
+<b>СЕМЬЯНИКА</b> - центр знакомств и создания семьи.
+</h1>
+	
+</div>
+</article>
